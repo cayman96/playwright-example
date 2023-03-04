580f1a769807490043dd251a8e9e10341518a6b2
kaju <cayman96@gmail.com>
2023-03-04 17:42:12 +0100

    initial commit

--- test_example.py
+++ test_example.py
@@ -1,21 +1,19 @@
+# biblioteka do regexów
 import re
+# import Page i expect'a z playwrighta
 from playwright.sync_api import Page, expect
 
 
-def test_homepage_has_Playwright_in_title_and_get_started_link_linking_to_the_intro_page(page: Page):
-    page.goto("https://playwright.dev/")
+# deklaracja funkcji z testu, jako że używamy pytesta to musi się zaczynać od test, tak samo plik się musi tak zaczynać
+def test_pierwszy_przejdzie(page: Page):
+    # przejście na stronę
+    page.goto("http://www.kurs-selenium.pl/demo/")
+    # pierwsza asercja, wymagany tytuł strony
+    expect(page).to_have_title(re.compile("PHPTRAVELS"))
+    '''jak używam funkcji get_by_role, to tutaj szukam wg... well, roli elementu, tutaj jest to link,
+    ale może być jako button, text, label, id, i tak dalej... odsyłam do dokumentacji jakby co'''
+    blog_btn = page.get_by_role("link", name="Blog")
+    expect(blog_btn).to_have_attribute("href", re.compile(".*/demo/blog"))
+    blog_btn.click()
+    expect(page).to_have_url(re.compile(".*/demo/blog"))
 
-    # Expect a title "to contain" a substring.
-    expect(page).to_have_title(re.compile("Playwright"))
-
-    # create a locator
-    get_started = page.get_by_role("link", name="Get started")
-
-    # Expect an attribute "to be strictly equal" to the value.
-    expect(get_started).to_have_attribute("href", "/docs/intro")
-
-    # Click the get started link.
-    get_started.click()
-
-    # Expects the URL to contain intro.
-    expect(page).to_have_url(re.compile(".*intro"))
